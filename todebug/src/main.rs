d15b599c69096e2558e9967e12c24c7891871d94
Tuyen Dang <tuyendq@gmail.com>
2026-04-03 17:06:07 +0700

    use .clone() to copy string

--- todebug/src/main.rs
+++ todebug/src/main.rs
@@ -14,9 +14,13 @@ fn main() {
     // let y = x;
     // println!("x: {}, y: {}", x, y);
 
-    let s1 = String::from("hello");
-    let s2 = s1;
-    // println!("s1: {}", s1);  // Error: value borrowed here after move
-    println!("s2: {}", s2);
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+    // // println!("s1: {}", s1);  // Error: value borrowed here after move
+    // println!("s2: {}", s2);
+
+    let s1 = String::from("learn Rust");
+    let s2 = s1.clone();
+    println!("s1: {}, s2: {}", s1, s2);
     
 }
