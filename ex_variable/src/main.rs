9e544edc3efb9d8d5fbe5267ea6c3444955c4abb
Tuyen Dang <tuyendq@gmail.com>
2026-04-18 20:17:42 +0700

    update ex_variable example to demonstrate variable shadowing instead of mutation

--- ex_variable/src/main.rs
+++ ex_variable/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let x: i32 = 5;  // immutable variable
     println!("Value of x is: {}", x);
-    x = 6;
+    // x = 10; // This will cause a compile-time error because x is immutable
+    let x = 10; // shadowing the previous variable x
     println!("Value of x is: {}", x);
 }
